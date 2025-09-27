8182c69a13df0aff8d718c55adfc5ec383916cab
Tunglies <Tunglies@users.noreply.github.com>
2025-09-27 14:51:13 +0000

    deploy: 24db8e1026630ccddd3e410c6553b02f31633ae5

--- trait.impl/core/marker/trait.Copy.js
+++ trait.impl/core/marker/trait.Copy.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["kode_bridge",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"kode_bridge/retry/enum.BackoffStrategy.html\" title=\"enum kode_bridge::retry::BackoffStrategy\">BackoffStrategy</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"kode_bridge/retry/enum.JitterStrategy.html\" title=\"enum kode_bridge::retry::JitterStrategy\">JitterStrategy</a>"]]]]);
+    var implementors = Object.fromEntries([["kode_bridge",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"kode_bridge/retry/enum.BackoffStrategy.html\" title=\"enum kode_bridge::retry::BackoffStrategy\">BackoffStrategy</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"kode_bridge/retry/enum.JitterStrategy.html\" title=\"enum kode_bridge::retry::JitterStrategy\">JitterStrategy</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"kode_bridge/ipc_http_server/struct.ServerConfig.html\" title=\"struct kode_bridge::ipc_http_server::ServerConfig\">ServerConfig</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[590]}
+//{"start":57,"fragment_lengths":[895]}
