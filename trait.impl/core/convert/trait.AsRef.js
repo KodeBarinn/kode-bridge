04909829d65a301c98765823f3337f3fea31ec88
Tunglies <Tunglies@users.noreply.github.com>
2026-08-20 08:53:11 +0000

    deploy: 0c6e583757dd23a6b36b6dfa6084778478a253bf

---
+++ trait.impl/core/convert/trait.AsRef.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["kode_bridge",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.87.0/std/path/struct.Path.html\" title=\"struct std::path::Path\">Path</a>&gt; for <a class=\"struct\" href=\"kode_bridge/struct.Endpoint.html\" title=\"struct kode_bridge::Endpoint\">Endpoint</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[419]}
---
+++ trait.impl/tokio/io/async_write/trait.AsyncWrite.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["kode_bridge",[["impl AsyncWrite for <a class=\"struct\" href=\"kode_bridge/struct.IpcStream.html\" title=\"struct kode_bridge::IpcStream\">IpcStream</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[158]}
